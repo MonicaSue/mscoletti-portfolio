53896f7e3861286d917c82b12e89ebd3ed02ef4e
Monica Sue <monicasue567@gmail.com>
2023-05-24 17:18:56 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import About from './sections/About';
+import Contact from './sections/Contact';
+import Resume from './sections/Resume';
+import Projects from './sections/Projects';
+import NavBar from './components/NavBar';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <About />
+    <Projects />
+    <Resume />
+    <Contact />
+    </>  
+  );
+}
+
+export default App;
